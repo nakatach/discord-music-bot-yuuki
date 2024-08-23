8bd3db51e3a0a323c9b82b4d1af6a0b942a168da
Nact <christiannakata9392@gmail.com>
2024-08-23 10:33:06 +0700

    Update yuuki.py

--- yuuki.py
+++ yuuki.py
@@ -1,87 +1,136 @@
 import nextcord
-import os
 import asyncio
 import yt_dlp
-from dotenv import load_dotenv
+from nextcord.ext import commands
 
-def run_bot():
-    load_dotenv()
-    TOKEN = os.getenv('bot_token')
-    intents = nextcord.Intents.default()
-    intents.message_content = True
-    intents.presences = True
-    intents.members = True
-    client = nextcord.Client(intents=intents)
+class MusicPlayer(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.voice_clients = {}
+        self.queue = {}
+        self.is_playing = {}
+        self.yt_dlp_options = {"format": "bestaudio/best"}
+        self.ytdl = yt_dlp.YoutubeDL(self.yt_dlp_options)
+        self.ffmpeg_options = {'options': '-vn -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'}
 
-    voice_clients = {}
-    yt_dlp_options = {"format" : "bestaudio/best"}
-    ytdl = yt_dlp.YoutubeDL(yt_dlp_options)
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'{self.bot.user} is now jamming')
 
-    ffmpeg_options = {'options' : '-vn'}
+    def search_youtube(self, query):
+        try:
+            result = self.ytdl.extract_info(f"ytsearch:{query}", download=False)
+            video = result['entries'][0]  # Ambil hasil pertama dari pencarian
+            return video['webpage_url'], video['title']
+        except Exception as e:
+            print(f"Error searching on YouTube: {e}")
+            return None, None
 
-    @client.event
-    async def on_ready():
-        print(f'{client.user} is now jamming')
+    async def play_next(self, ctx):
+        if len(self.queue[ctx.guild.id]) > 0:
+            url, title = self.queue[ctx.guild.id].pop(0)
+            await self.play_song(ctx, url, title)
+        else:
+            self.is_playing[ctx.guild.id] = False
 
-    @client.event
-    async def on_message(message):
-        if message.content.startswith("/play"):
-            if not message.author.voice:
-                await message.channel.send("You need to be connected to a voice channel to play music.")
-                return
-            
+    async def play_song(self, ctx, url, title):
+        try:
+            loop = asyncio.get_event_loop()
+            data = await loop.run_in_executor(None, lambda: self.ytdl.extract_info(url, download=False))
+
+            song = data['url']
+            print(f"Playing song: {song}")
+            player = nextcord.FFmpegPCMAudio(song, **self.ffmpeg_options)
+
+            self.voice_clients[ctx.guild.id].play(player, after=lambda e: asyncio.run_coroutine_threadsafe(self.play_next(ctx), loop))
+            await ctx.send(f"Now playing: {title}")
+        except Exception as e:
+            print(f"Error playing the song: {e}")
+            await ctx.send("Error playing the song.")
+
+    @commands.command(name="play")
+    async def play(self, ctx, *, search: str):
+        if not ctx.author.voice:
+            await ctx.send("You need to be connected to a voice channel to play music.")
+            return
+
+        if ctx.guild.id not in self.queue:
+            self.queue[ctx.guild.id] = []
+
+        if ctx.guild.id not in self.is_playing:
+            self.is_playing[ctx.guild.id] = False
+
+        if ctx.guild.id not in self.voice_clients or not self.voice_clients[ctx.guild.id].is_connected():
             try:
-                # Disconnect from voice channel if already connected
-                if message.guild.id in voice_clients and voice_clients[message.guild.id].is_connected():
-                    await voice_clients[message.guild.id].disconnect()
-                
-                voice_client = await message.author.voice.channel.connect()
-                voice_clients[voice_client.guild.id] = voice_client
+                voice_client = await ctx.author.voice.channel.connect()
+                self.voice_clients[voice_client.guild.id] = voice_client
                 print("Connected to voice channel")
             except Exception as e:
                 print(f"Error connecting to the voice channel: {e}")
-                await message.channel.send("Error connecting to the voice channel.")
+                await ctx.send("Error connecting to the voice channel.")
                 return
 
-            try:
-                url = message.content.split()[1]
+        if not search.startswith("http"):
+            url, title = self.search_youtube(search)
+            if url is None:
+                await ctx.send("Could not find the song on YouTube.")
+                return
+        else:
+            url = search
+            title = "Playing from provided URL"
 
-                if not url:
-                    await message.channel.send("Please provide a valid YouTube URL.")
-                    return
-                
-                print(f"Fetching info for URL: {url}")
+        self.queue[ctx.guild.id].append((url, title))
+        await ctx.send(f"Added to queue: {title}")
 
-                loop = asyncio.get_event_loop()
-                data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+        if not self.is_playing[ctx.guild.id]:
+            self.is_playing[ctx.guild.id] = True
+            await self.play_next(ctx)
 
-                song = data['url']
-                print(f"Playing song: {song}")
-                player = nextcord.FFmpegPCMAudio(song, **ffmpeg_options)
+    @commands.command(name="pause")
+    async def pause(self, ctx):
+        try:
+            self.voice_clients[ctx.guild.id].pause()
+        except Exception as e:
+            print(e)
 
-                voice_clients[message.guild.id].play(player)
-                await message.channel.send(f"Now playing: {data['title']}")
-            except Exception as e:
-                print(f"Error playing the song: {e}")
-                await message.channel.send("Error playing the song.")
+    @commands.command(name="resume")
+    async def resume(self, ctx):
+        try:
+            self.voice_clients[ctx.guild.id].resume()
+        except Exception as e:
+            print(e)
 
-        if message.content.startswith("/pause"):
-            try:
-                voice_clients[message.guild.id].pause()
-            except Exception as e:
-                print(e)
+    @commands.command(name="skip")
+    async def skip(self, ctx):
+        if ctx.guild.id in self.voice_clients:
+            self.voice_clients[ctx.guild.id].stop()
+            await self.play_next(ctx)
 
-        if message.content.startswith("/resume"):
-            try:
-                voice_clients[message.guild.id].resume()
-            except Exception as e:
-                print(e)
+    @commands.command(name="stop")
+    async def stop(self, ctx):
+        try:
+            self.voice_clients[ctx.guild.id].stop()
+            await self.voice_clients[ctx.guild.id].disconnect()
+            self.queue[ctx.guild.id] = []
+            self.is_playing[ctx.guild.id] = False
+        except Exception as e:
+            print(e)
 
-        if message.content.startswith("/stop"):
-            try:
-                voice_clients[message.guild.id].stop()
-                await voice_clients[message.guild.id].disconnect()
-            except Exception as e:
-                print(e)
+    @commands.command(name="q")
+    async def show_queue(self, ctx):
+        if ctx.guild.id in self.queue and self.queue[ctx.guild.id]:
+            queue_str = "\n".join([f"{i+1}. {title}" for i, (_, title) in enumerate(self.queue[ctx.guild.id])])
+            await ctx.send(f"Current queue:\n{queue_str}")
+        else:
+            await ctx.send("The queue is empty.")
 
-    client.run(TOKEN)
+    @commands.command(name="remove")
+    async def remove(self, ctx, index: int):
+        if ctx.guild.id in self.queue and len(self.queue[ctx.guild.id]) >= index > 0:
+            removed_song = self.queue[ctx.guild.id].pop(index - 1)
+            await ctx.send(f"Removed from queue: {removed_song[1]}")
+        else:
+            await ctx.send("Invalid index or empty queue.")
+
+def setup(bot):
+    bot.add_cog(MusicPlayer(bot))
